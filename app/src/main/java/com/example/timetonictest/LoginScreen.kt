f75f11e097db6353fde402fa71397e7e95607379
Santiago Sáchica <ssachica@gmail.com>
2024-05-29 18:16:38 -0500

    Implement login interface
    This commit adds the initial implementation of the login interface in the app.
    The login screen includes TimeTonic logo, input fields for email and password, and a Login button.
    
    Changes Made:
    - Created the login layout XML file.
    - Added the company logo.
    - Added TextInputLayouts for email and password.
    - Styled the input fields and button.
    - Added some res values like colors, strings, and styles.

--- app/src/main/java/com/example/timetonictest/LoginScreen.kt
+++ app/src/main/java/com/example/timetonictest/LoginScreen.kt
@@ -1,15 +1,20 @@
 package com.example.timetonictest
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_login_screen)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
